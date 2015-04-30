ed38dfb2ba81c189371684fe623056546767d4aa
Endre Bock <dev@itbock.de>
2015-04-30 19:49:38 +0200

    Code style corrections and extending documentation.

--- src/core.js
+++ src/core.js
@@ -8,10 +8,12 @@
  
  
  
- /**
+/**
  * DOM Implementation of event listener and target.
  * The class is mainly a wrapper of an XMLElement to get the DOM event handling
  * implementation available.
+ * 
+ * Thread-safe: no.
  *
  * @constructor
  * @author Endre Bock <corejs-w3c@itbock.de>
@@ -21,45 +23,55 @@ function DOMEventListener()
 	/**
 	 * DOM implementation scope object.
 	 * @type {HTMLElement}
-	 * @private
+	 * @protected
 	 */
-	var _dom = document.createElement('xml');
-	_dom['scope'] = this;
-
-	/**
-	 * Use DOM implementation.
-	 *
-	 * @param event
-	 * @param listener
-	 * @param useCapture
-	 */
-	this.addEventListener = function(event, listener, useCapture)
-	{
-		_dom.addEventListener(event, listener, useCapture);
-	};
-
-	/**
-	 * Use DOM implementation.
-	 *
-	 * @param event
-	 * @param listener
-	 * @param useCapture
-	 */
-	this.removeEventListener = function(event, listener, useCapture)
-	{
-		_dom.removeEventListener(event, listener, useCapture);
-	};
-
-	/**
-	 * Use DOM implementation.
-	 *
-	 * @param event
-	 */
-	this.dispatchEvent = function(event)
-	{
-		console.log("Dispatch event:", event.type, event);
-		_dom.dispatchEvent(event);
-	};
+	this._dom = document.createElement('xml');
+	this._dom['scope'] = this;
 }
 DOMEventListener.prototype = Object.create(EventTarget.prototype);
 DOMEventListener.prototype.constructor = DOMEventListener;
+
+/**
+ * The EventTarget.addEventListener() method registers the specified listener
+ * on the EventTarget it's called on. The event target may be an Element in a
+ * document, the Document itself, a Window, or any other object that supports 
+ * events (such as XMLHttpRequest).
+ *
+ * @param {String} event - The event type.
+ * @param {(Function|Object)} listener - Observer object.
+ * @param {Boolean}  useCapture - Capture event flag.
+ */
+DOMEventListener.prototype.addEventListener = 
+function(event, listener, useCapture)
+{
+	this._dom.addEventListener(event, listener, useCapture);
+};
+
+/**
+ * Removes the event listener previously registered with 
+ * EventTarget.addEventListener().
+ *
+ * @param {String} event - The event type.
+ * @param {(Function|Object)} listener - Observer object.
+ * @param {Boolean}  useCapture - Capture event flag.
+ */
+DOMEventListener.prototype.removeEventListener =
+function(event, listener, useCapture)
+{
+	this._dom.removeEventListener(event, listener, useCapture);
+};
+
+/**
+ * Dispatches an Event at the specified EventTarget, invoking the affected 
+ * EventListeners in the appropriate order. The normal event processing rules 
+ * (including the capturing and optional bubbling phase) apply to events 
+ * dispatched manually with dispatchEvent().
+ *
+ * @param {CustomEvent} event - Event object to dispatch.
+ */
+DOMEventListener.prototype.dispatchEvent =
+function(event)
+{
+	console.log("Dispatch event:", event.type, event);
+	this._dom.dispatchEvent(event);
+};
