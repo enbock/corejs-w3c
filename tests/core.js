d3679c9b39acf756d507750a7cf53a632f2db28c
Endre Bock <dev@itbock.de>
2015-05-09 16:36:05 +0200

    add missing function

--- tests/core.js
+++ tests/core.js
@@ -8,31 +8,33 @@ var sinon = require("sinon");
 /**
  * DOM stack
  */
-global.document    = {};
-global.EventTarget = function () {};
+global.document = {
+	createElement: function () { }
+};
+global.EventTarget = function () { };
 
 require("../src/core.js");
 
-describe("DOMEventListener", function() {
+describe("DOMEventListener", function () {
 	var testObject;
 	var documentMock;
 	var domMock;
 
-	beforeEach(function() {
-		domMock      = sinon.mock();
+	beforeEach(function () {
+		domMock = sinon.mock();
 		documentMock = sinon.mock(document);
 		documentMock.expects("createElement")
 			.once().withExactArgs('xml').returns(domMock);
-		testObject = new DOMEventListener();		
+		testObject = new DOMEventListener();
 	});
-	
-	afterEach(function() {
+
+	afterEach(function () {
 		domMock.verify();
 		documentMock.verify();
 	});
-	
-	describe("#addEventListener", function() {
-		it("should all dom addEventListener", function() {
+
+	describe("#addEventListener", function () {
+		it("should all dom addEventListener", function () {
 			domMock.expects("addEventListener").once().withExactArgs(1, 2, 3);
 			testObject.addEventListener(1, 2, 3);
 		});
